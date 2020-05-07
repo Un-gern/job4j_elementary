9f25d7f80d34f5e15181e8ba6d19f5794143f955
Un-gern <deniz.baikal@gmail.com>
2020-05-07 20:32:10 +0400

    3.3. Разбор первой программы. [#286207]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Petr Arsetev");
+        System.out.println("06.01.2019");
+
+
+    }
+}
