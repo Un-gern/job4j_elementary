1d040ccb9db1d0d15836205e2b0d9b607e429eb2
Un-gern <deniz.baikal@gmail.com>
2020-05-16 17:19:16 +0400

    Тесты для определения расстояния между точками. [#286225]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -10,7 +10,7 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
+        double result1 = SqArea.square(4, 1);
         System.out.println(" p = 6, k = 2, s = " + result1);
     }
 }
