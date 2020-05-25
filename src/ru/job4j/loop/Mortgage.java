2f449b7f6ebf2d57556ff3d580ca2ce8ae7a4f8f
Un-gern <Zxkt6ahj>
2020-05-25 16:01:57 +0400

    Ипотека. [#286239]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,14 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
-        if (amount != 0) {
-            double bal = amount * percent / 100.0 + amount - salary;
-            year = 1;
-            while (bal > 0) {
-                bal = bal * percent / 100.0 + bal - salary;
+                while (amount > 0) {
+                amount = (int) (amount * percent / 100.0 + amount - salary);
                 year++;
             }
-        }
+
 
         return year;
     }
