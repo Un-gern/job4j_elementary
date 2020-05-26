173a8088ac35e5241745bded0c7295dba93274e9
Un-gern <Zxkt6ahj>
2020-05-26 19:19:38 +0400

    6.0.2. Заполнение массива. [#286176]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,8 +2,16 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] summaries = new String[100500];
-        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Maksim Antonov";
+        names[2] = "Petr Petrov";
+        names[3] = "Sidor Sidorov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+
     }
 }
